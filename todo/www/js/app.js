e415326af993efcce8cdfd9a966c68692e3d6456
Wan Kim Mok <cafe.mui@gmail.com>
2015-06-30 22:41:40 -0400

    include todo controller

--- todo/www/js/app.js
+++ todo/www/js/app.js
@@ -1,1 +1,32 @@
 angular.module('todo', ['ionic'])
+
+.controller('TodoCtrl', function($scope, $ionicModal){
+     //task list
+     $scope.tasks = [];
+
+     //create and load the modal
+     $ionicModal.fromTemplateUrl('new-task.html', function(modal){
+          $scope.taskModal = modal;
+     }, {
+          scope: $scope,
+          animation: 'slide-in-up'
+     });
+
+     //when task is created
+     $scope.createTask = function(task){
+          $scope.tasks.push({
+               title: task.title
+          });
+          $scope.taskModal.hide();
+          task.title = "";
+     };
+
+     //open task modal 
+     $scope.newTask = function(){
+          $scope.taskModal.show();
+     }
+     //cancel task
+     $scope.closeNewTask = function(){
+          $scope.taskModal.hide();
+     };
+});
